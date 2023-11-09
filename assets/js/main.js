a1979bd5c18145ff3c3188cdb8fb2bd96a103b67
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-11-09 00:15:11 -0300

    Organizando melhor o Código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-const offset = 0;
-const limit = 10;
+    const offset = 0;
+    const limit = 10;
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -25,18 +25,10 @@ function convertPokemonToLi(pokemon) {
         </ol>`
 }
 
+const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemonToLi(pokemon))
-            
-        }
+PokeApi.getPokemons().then((pokemons = []) => {  
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
 
-    })
-    .catch((error) => console.log(error))
-    
+
