d05b1bfc7482cea7d668992c4e34cdc43ef19b3e
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-12-11 12:31:14 -0300

    Atualizando a seleção dinamcia

--- assets/js/pokeAPI.js
+++ assets/js/pokeAPI.js
@@ -1,10 +1,39 @@
 
 const PokeApi = {}
 
-PokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+
+}
+
+PokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+
+}
+
+PokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;  
+
     return fetch(url)  
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
